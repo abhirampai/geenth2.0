0118e1624b8dd0ec2d543dcffe3ac230387c797c
Pai026 <abhirampai1999@gmail.com>
2021-08-30 16:21:13 +0530

    added functionality to change music on clicking playlist

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import "./App.css";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
 import { songData } from "./api/songs";
+import MusicBar from "./Components/MusicBar";
+import { useState } from "react";
 function App() {
   const options = {
     audioLists: songData,
+    mode: "mini",
   };
-
+  const [audioInstance, setAudioInstance] = useState(null);
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
-    <ReactJkMusicPlayer {...options} />
+    <>
+      <ReactJkMusicPlayer
+        {...options}
+        getAudioInstance={(instance) => {
+          setAudioInstance(instance);
+        }}
+      />
+      <MusicBar AudioInstance={audioInstance} />
+    </>
   );
 }
 
